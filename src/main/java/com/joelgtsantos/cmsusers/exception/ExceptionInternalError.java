ddb9aa886c97136d551decd55b2d277ca4d30893
Admiral-Simo <admiralsimo16@gmail.com>
2025-08-29 17:02:37 +0100

    My commit

--- src/main/java/com/joelgtsantos/cmsusers/exception/ExceptionInternalError.java
+++ src/main/java/com/joelgtsantos/cmsusers/exception/ExceptionInternalError.java
@@ -5,7 +5,7 @@ public class ExceptionInternalError extends Exception {
 	
 	private static final long serialVersionUID = -7539901274547128652L;
 	
-	public static final String NOT_FOUND="Entity not found {}";
+	public static final String NOT_FOUND="Entity not found {} something else";
 	public static final String CONFLICT="Entity conflict";
 
 	public ExceptionInternalError() {
